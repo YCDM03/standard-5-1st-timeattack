3b7826145974839217bcf3486aab13cb655842de
YCDM03 <ycdm03@gmail.com>
2024-05-20 15:53:16 +0900

    d2

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ export default function App() {
         onSubmit={(e) => {
           e.preventDefault();
           e.target[0].value = "";
+          setInput("");
         }}
       >
         <input
